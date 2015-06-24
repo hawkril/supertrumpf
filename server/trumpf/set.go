6ee9b67fa8c3f3cfbe4d4c8c1fb84f42f1741dd1
Tim Scheuermann <tim@plusmid.net>
2015-06-24 11:37:22 +0200

    Added possibility to query all sets

--- server/trumpf/set.go
+++ server/trumpf/set.go
@@ -1,36 +1,53 @@
 package trumpf
 
 import (
+	"encoding/xml"
 	"errors"
-	"strconv"
 	"strings"
 
 	"trumpf-core/database"
 )
 
 type Set struct {
-	ID    string `xml:"id"`
-	Count int    `xml:"count"`
+	XMLName xml.Name `xml:"set"`
+
+	ID         string `xml:"name,attr"`
+	Title      string `xml:"title"`
+	CardCount  int    `xml:"card_count"`
+	MaxPlayers int    `xml:"max_players"`
 }
 
 func QuerySet(id string) (*Set, error) {
 	// ToDo: Write an adequate escape
 	id = strings.Replace(id, `"`, ``, -1)
 
-	rows, err := database.Query(`for $x in doc("testdata1/sets.xml")/sets/set where $x/@name="` + id + `" return data($x/card_count)`)
+	rows, err := database.Query(`for $x in doc("testdata1/sets.xml")/sets/set where $x/@name="` + id + `" return $x`)
 	if err != nil {
 		return nil, err
 	}
 	if len(rows) == 0 {
 		return nil, errors.New("Set not found")
 	}
+	s := &Set{}
+	err = xml.Unmarshal([]byte(rows[0]), s)
+	return s, err
+}
 
-	i, err := strconv.ParseInt(rows[0], 10, 64)
+func QueryAllSets() ([]*Set, error) {
+	rows, err := database.Query(`for $x in doc("testdata1/sets.xml")/sets/set return $x`)
 	if err != nil {
 		return nil, err
 	}
-	return &Set{
-		ID:    id,
-		Count: int(i),
-	}, nil
+	if len(rows) == 0 {
+		return nil, errors.New("No sets found")
+	}
+	result := make([]*Set, len(rows))
+	for i, row := range rows {
+		s := &Set{}
+		if err = xml.Unmarshal([]byte(row), s); err != nil {
+			return nil, err
+		}
+		result[i] = s
+	}
+	return result, nil
 }
