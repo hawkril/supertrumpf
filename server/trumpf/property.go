ffe92126828442f7b683527bc081351cae5e83ea
hawkril <torsten.spickhofen@gmail.com>
2015-07-05 23:49:21 +0200

    Bug fixes

--- server/trumpf/property.go
+++ server/trumpf/property.go
@@ -7,7 +7,7 @@ import (
 type PropertyDef struct {
 	XMLName xml.Name `xml:"value"`
 
-	ID   int    `xml:"tag"`
+	ID   string    `xml:"tag"`
 	Name string `xml:"name"`
 	Unit string `xml:"suffix"`
 	Type string `xml:"type"`
